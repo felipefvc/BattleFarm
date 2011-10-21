6d255d565b82b5436986f6d1525697e06fd631c8
thalesmello <thalesmello@gmail.com>
2011-10-21 15:06:07 -0200

    Primeiro commit do BattleFarmTest

---
+++ BattleFarmTest/src/br/ita/battlefarm/test/BFTest.java
@@ -0,0 +1,13 @@
+package br.ita.battlefarm.test;
+
+import br.ita.battlefarm.BattleFarmActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class BFTest extends ActivityInstrumentationTestCase2<BattleFarmActivity> {
+
+	public BFTest(Class<BattleFarmActivity> activityClass) {
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
+
+}
